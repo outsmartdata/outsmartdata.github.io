cfb7e67418faa01fb64fcb860d8f2dc3166f6534
wkoslik <wkoslik@gmail.com>
2021-10-03 20:33:29 -0400

    adding footer and header partial

---
+++ outsmart-data/pages/partials/footer.js
@@ -0,0 +1,5 @@
+export default function Foooter () {
+    return (
+        <h1>I am the footer</h1>
+    )
+}
---
+++ outsmart-data/pages/partials/header.js
@@ -0,0 +1,5 @@
+export default function Header () {
+    return(
+        <h1>I am the header</h1>
+    )
+}
